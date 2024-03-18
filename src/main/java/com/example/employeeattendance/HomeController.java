2f9e99d1c9d7d22fd4f73c7fd95a31cec4b4f585
Sleepapp <vontthao@gmail.com>
2024-03-18 20:49:45 +0700

    Chỉnh lại lỗi đường dẫn

---
+++ src/main/java/com/example/employeeattendance/HomeController.java
@@ -0,0 +1,37 @@
+package com.example.employeeattendance;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+
+public class HomeController {
+    @FXML
+    private Button dashboard;
+
+    @FXML
+    private Button dashboard1;
+
+    @FXML
+    private Button dashboard11;
+
+    @FXML
+    private Pane inner_pane;
+
+    @FXML
+    private Pane inner_pane1;
+
+    @FXML
+    private Pane inner_pane2;
+
+    @FXML
+    private Pane inner_pane21;
+
+    @FXML
+    private Pane inner_pane211;
+
+    @FXML
+    private HBox root;
+
+
+}
