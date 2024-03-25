4a891da53ff043310a19da0b3a7cf8a9694b127f
Brandy Tran <baotutran1103@gmail.com>
2024-03-25 20:37:55 +0700

    Add table

--- src/main/java/Admin/AdminController.java
+++ src/main/java/Admin/AdminController.java
@@ -6,6 +6,11 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,6 +18,14 @@ import java.util.ResourceBundle;
 public class AdminController implements Initializable {
     @FXML
     private PieChart pieChart;
+    @FXML
+    private AnchorPane background;
+    @FXML
+    private Button HomeButton;
+    @FXML
+    private ChoiceBox<?> MonthButton;
+
+    //Data for PieChart
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         ObservableList<PieChart.Data> pieChartData =
@@ -33,4 +46,26 @@ public class AdminController implements Initializable {
         //making the legend invisible
         pieChart.setLegendVisible(false);
     }
+
+    @FXML
+    private TableView<?> table;
+
+    @FXML
+    private TableColumn<?, ?> Absent;
+
+    @FXML
+    private TableColumn<?, ?> Approve;
+
+    @FXML
+    private TableColumn<?, ?> ID;
+
+    @FXML
+    private TableColumn<?, ?> Late;
+
+    @FXML
+    private TableColumn<?, ?> Overtime;
+
+    @FXML
+    private TableColumn<?, ?> Status;
+
 }
